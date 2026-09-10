6f2b39bfddbe3b6b80309cb2def2a305d114a95b
davejones2357 <dave.jones.2357@gmail.com>
2026-09-10 22:50:10 +0100

    invokes methods from module_provider

--- go.mod
+++ go.mod
@@ -1,4 +1,7 @@
 module module_user
 
-
 go 1.26.2
+
+require module_provider v0.0.0
+
+replace module_provider => ../module_provider
--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"module_provider/foo"
+	"module_provider/bar"
+)
 
 func main() {
-    fmt.Println("hello world")
+	fmt.Println("hello world")
+	fmt.Printf("hello %s\n", foo.Fooit())
+	fmt.Printf("hello %s\n", bar.Barit())
 }
